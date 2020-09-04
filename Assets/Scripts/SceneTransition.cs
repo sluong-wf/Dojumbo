0e64851fd41cfbad3a0c0676d6a08a377ed30cbc
Shayla Luong <luongnh1@uci.edu>
2020-09-03 23:46:26 -0700

    Add fading when transitioning between scenes

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,11 +8,34 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    // called before Start
+    void Awake() {
+        if(fadeInPanel != null) {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
     
     public void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player") && !other.isTrigger) {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            // SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+        }
+    }
+
+    public IEnumerator FadeCo() { // Coroutine
+        if(fadeOutPanel != null) {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while(!asyncOperation.isDone) {
+            yield return null;
         }
     }
 }
