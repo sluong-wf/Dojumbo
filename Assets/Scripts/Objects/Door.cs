8ea93589a4aa5996415698e054f70962f43c5ba7
Shayla Luong <luongnh1@uci.edu>
2020-09-09 17:24:24 -0700

    Add room transition behavior

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    // [SerializeField] private Animator anim; 
+    
+    public GameObject roomTransition;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange) {
+            EnableInactive();
+            // anim.SetBool("Open", true);
+            roomTransition.SetActive(true);
+        }
+    }
+
+    public override void OnTriggerExit2D(Collider2D other) {
+        base.OnTriggerExit2D(other);
+        if(other.CompareTag("Player")) {
+            // anim.SetBool("Open", false);
+            roomTransition.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/RoomTransition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+    public Vector2 playerPosition;
+    public VectorValue playerStorage;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+    
+    [SerializeField] private GameObject currCam;
+    [SerializeField] private GameObject nextCam;
+
+    private PlayerMovement player;
+
+    void Start() {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+    }
+    
+    void OnEnable() {
+        playerStorage.InitialValue = playerPosition;
+        StartCoroutine(FadeCo());
+    }
+
+    public IEnumerator FadeCo() {
+        currCam.SetActive(false);
+        nextCam.SetActive(true);
+
+        if(fadeInPanel != null) {
+            player.currentState = PlayerState.interact;
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        
+        player.currentState = PlayerState.idle;
+    }
+}
